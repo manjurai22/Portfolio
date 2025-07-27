300f39b943f0ca9e36f7bb26f988beff68025843
manjurai22 <manjurai611@gmail.com>
2025-07-27 22:47:00 +0545

    forms

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from portfolio_app import views
 urlpatterns=[
     path("",views.portfolio,name="portfolio"),
-      path("project/",views.project_list, name="project-list"),
+    path("project/",views.project_list, name="project-list"),
+    path("project-create/",views.project_list,name="project-create"),
 ]
