ee933f95cb8613878f19cf36ca5ff9cd1f599000
manjurai22 <manjurai611@gmail.com>
2025-07-27 22:29:30 +0545

    create

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from portfolio_app.models import Project
+from protfolio_app.forms import ProjectForm
 
 def portfolio(request):
     projects=Project.objects.all()
@@ -18,3 +19,19 @@ def project_list(request):
         'project_list.html',
         {'projects': projects}
     )
+
+def project_create(request):
+    if request.method == "GET":
+        form = ProjectForm()
+        return render(
+            request,
+            "project_create.html",
+            {"form": form},
+            )
+    else:
+        form = ProjectForm(request.POST)
+
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.save()
+            return redirect("project-list")
